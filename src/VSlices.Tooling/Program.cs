e77d09ce1a5fb50ca52d05704d4d32d8d5910756
Hernan Alvarez <h.f.alvarez.rubio@gmail.com>
2026-06-24 16:48:55 -0400

    feat(v0.1): inicio del tooling!

--- src/VSlices.Tooling/Program.cs
+++ src/VSlices.Tooling/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var app = ConsoleApp.Create();
+
+app.Add("generate-document", (
+    string type,
+    string level = "L0",
+    string language = "en") =>
+{
+    Console.WriteLine($"Generating {language} {level} {type} document.");
+});
+
+app.Run(args);
