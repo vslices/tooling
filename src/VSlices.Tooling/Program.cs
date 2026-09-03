e183e622ada859be8ad617661c3556016f41e779
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-03 15:50:19 -0400

    Add lower orchestration command

--- src/VSlices.Tooling/Program.cs
+++ src/VSlices.Tooling/Program.cs
@@ -3,6 +3,7 @@
 var app = ConsoleApp.Create();
 
 app.Add("init", RulesetCommands.Init);
+app.Add("lower", VsirCommands.Lower);
 app.Add("transpile", VsirCommands.Transpile);
 app.Add("rebase", VsirCommands.Rebase);
 
