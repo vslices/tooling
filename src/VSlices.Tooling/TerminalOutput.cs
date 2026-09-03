ad02a34f1d3f42a8818f7590e56fe3c741cfd1a8
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-03 18:47:30 -0400

    Add official VSlices terminal branding

--- src/VSlices.Tooling/TerminalOutput.cs
+++ src/VSlices.Tooling/TerminalOutput.cs
@@ -5,6 +5,39 @@ namespace VSlices.Tooling;
 
 internal static class TerminalOutput
 {
+    private static readonly AnsiStyle BrandStyle = Chalk.Rgb(156, 47, 160);
+
+    public static void Brand(string? context = null)
+    {
+        if (Console.IsOutputRedirected)
+            return;
+
+        var logo = new[]
+        {
+            @"\ \       / /",
+            @" \ \     / / ",
+            @"  \ \   / /  ",
+            @"   \ \ / /   ",
+            @"    \ V /    ",
+            @"     \ /     ",
+            @"      V      "
+        };
+
+        for (var index = 0; index < logo.Length; index++)
+        {
+            var suffix = index switch
+            {
+                1 => "  " + Chalk.Bold["VSlices"],
+                2 when !string.IsNullOrWhiteSpace(context) => "  " + Chalk.Gray[context],
+                _ => string.Empty
+            };
+
+            Console.WriteLine(BrandStyle[logo[index]] + suffix);
+        }
+
+        BlankLine();
+    }
+
     public static void Heading(string text) =>
         Console.WriteLine(Chalk.Bold[text]);
 
