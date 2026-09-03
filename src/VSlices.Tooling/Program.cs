6741497ba5b3b8a3d5edb9ad6bece4c09921451b
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-03 16:23:04 -0400

    Register update command

--- src/VSlices.Tooling/Program.cs
+++ src/VSlices.Tooling/Program.cs
@@ -6,5 +6,6 @@ app.Add("init", RulesetCommands.Init);
 app.Add("lower", VsirCommands.Lower);
 app.Add("transpile", VsirCommands.Transpile);
 app.Add("rebase", VsirCommands.Rebase);
+app.Add("update", UpdateCommands.Update);
 
 app.Run(args);
