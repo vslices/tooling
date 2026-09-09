c2bb1709c64a11da4d11f2ce7920ac32c7aa98d6
Hernan Alvarez <51165159+HernanFAR@users.noreply.github.com>
2026-09-09 08:25:42 -0300

    Desugar final Req pipeline transform

--- src/VSlices.Vsir.CSharp/CSharpLanguageLowerer.cs
+++ src/VSlices.Vsir.CSharp/CSharpLanguageLowerer.cs
@@ -118,7 +118,7 @@ public static class CSharpLanguageLowerer
         {
             for (var i = 0; i < pipeline.Count; i++)
                 source.AppendLine((i == 0 ? "        " : "        >> ") + pipeline[i]);
-            source.AppendLine($"        * (({inputType} input) => Instance(input));");
+            source.AppendLine($"        >> VSlices.Arrows.Req<{inputType}, {document.Name}>.Transform(({inputType} input) => Instance(input));");
         }
 
         source.AppendLine();
